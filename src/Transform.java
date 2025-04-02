44a2b48f7022f4fc85fc0c2b7777c3872793a82f
iamValen <valentimk04@gmail.com>
2025-04-02 17:44:23 +0100

    UML and Transform comment

--- src/Transform.java
+++ src/Transform.java
@@ -1,3 +1,10 @@
+/**
+ * Contém toda a informaçao da posicao de um GameObject
+ * @author Alexandre Menino a83974
+ * @author Grégory Endrio Leite a90952
+ * @author Valentim Khakhitva a81785
+ * @version 28/03/2025
+ */
 public class Transform implements ITransform {
     private Point posicao;
     private int layer;
