d72fd6570578614357f60dc472e086162eac4270
Alexandre <alexandre.menino05@gmail.com>
2025-04-26 19:00:56 +0100

    separated tests

--- src/IBehaviourTests.java
+++ src/IBehaviourTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 public class IBehaviourTests {
 
+
+
     class testBehaviour implements IBehaviour{
         boolean onInitRan = false;
         boolean onEnableRan = false;
@@ -19,28 +21,73 @@ public class IBehaviourTests {
     }
 
 
-
-    @Test
-    public void onEverythingExceptColisionTest(){ //onColision just runs everytime update runs
-        GameEngine ge = new GameEngine();
+    GameObject exampleGameObject(testBehaviour behaviour){
         Point[] pts1 = new Point[4];
         pts1[0] = new Point(1, 1);
         pts1[1] = new Point(1, 3);
         pts1[2] = new Point(3, 3);
         pts1[3] = new Point(3, 1);
         Polygon square1 = new Polygon(pts1);
+        return new GameObject("Square", 2, 2, 0, 0, 1, square1, behaviour);
+    }
+
+
+
+    @Test
+    public void oninitTest0(){
+        GameEngine ge = new GameEngine();
         testBehaviour behaviour = new testBehaviour();
-        GameObject go1 = new GameObject("Square", 2, 2, 0, 0, 1, square1, behaviour);
+        GameObject go1 = exampleGameObject(behaviour);
 
         ge.addEnabled(go1);
         assertEquals(true, behaviour.onInitRan);
+    }
+
+    @Test
+    public void oninitTest1(){
+        GameEngine ge = new GameEngine();
+        testBehaviour behaviour = new testBehaviour();
+        GameObject go1 = exampleGameObject(behaviour);
+
+        ge.addDisabled(go1);
+        assertEquals(true, behaviour.onInitRan);
+    }
+
+
+    @Test
+    public void onDisableTest(){
+        GameEngine ge = new GameEngine();
+        testBehaviour behaviour = new testBehaviour();
+        GameObject go1 = exampleGameObject(behaviour);
+
+        ge.addEnabled(go1);
         ge.disable(go1);
         assertEquals(true, behaviour.onDisableRan);
+    }
+
+
+    
+    @Test
+    public void onEnableTest(){
+        GameEngine ge = new GameEngine();
+        testBehaviour behaviour = new testBehaviour();
+        GameObject go1 = exampleGameObject(behaviour);
+
+        ge.addDisabled(go1);
         ge.enable(go1);
         assertEquals(true, behaviour.onEnableRan);
+    }
+
+
+    
+    @Test
+    public void onDestroyTest(){
+        GameEngine ge = new GameEngine();
+        testBehaviour behaviour = new testBehaviour();
+        GameObject go1 = exampleGameObject(behaviour);
+
+        ge.addEnabled(go1);
         ge.destroy(go1);
         assertEquals(true, behaviour.onDestroyRan);
-        
-
-    }    
+    }
 }
